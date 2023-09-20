47b99867e86ebd4d92db14a7f4e9c25f90168d45
hafiz <hafiz@gmail.com>
2023-09-20 15:16:24 +0400

    first commit

---
+++ src/assets/fonts/inconsolata/static/icons/index.jsx
@@ -0,0 +1,21 @@
+export const PlayIcon = () => {
+
+    const a=new SpeechSynthesisUtterance()
+
+    window.speechSynthesis.speak("dfskmld")
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="75"
+      height="75"
+      viewBox="0 0 75 75"
+      className="dark:bg-purple-800 bg-purple-300"
+    >
+      <g fill="#A445ED" fill-rule="evenodd">
+        <circle cx="37.5" cy="37.5" r="37.5" opacity=".25" />
+        <path d="M29 27v21l21-10.5z" />
+      </g>
+    </svg>
+  );
+};
